f00cbe92a68615d2ab29900b1b2eb32bf586297f
SHREYA RAHUL VARNE <shreyavarne99@gmail.com>
2025-07-07 15:46:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+
+harcascade = "model\haarcascade_frontalface_default.xml"
+cap = cv2.VideoCapture(0)
+cap.set(3,640) #width
+cap.set(4,480) #height
+while True:
+    success, img = cap.read()
+
+    facecascade = cv2.CascadeClassifier(harcascade)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    face = facecascade.detectMultiScale(img_gray, 1.1,4)
+
+    for(x,y,w,h) in face:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+    cv2.imshow("Face", img) #to get gray image => img_gray
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):  #if you press 'q' from keyboard , then your camera will be closed
+        break
